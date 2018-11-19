d838c69d5598e5bb262e94f9ce8b6c193f3a7b61
Adrian Zmuda <adi120.a@gmail.com>
2018-11-19 01:28:45 +0100

    Updeate Events , added scroreboard and improved project

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ canvas.height=350;
 ctx.fillStyle="beige";
 ctx.fillRect(0,0,800,350);
 let sI=null;
+let position=100;
 
 //BALL
 
@@ -13,8 +14,8 @@ const Ball = function(ctx,size,color){
     this.size=size;
     this.positionX=(ctx.canvas.width/2);
     this.positionY=(ctx.canvas.height/2*.4);
-    this.gX=5;
-    this.gY=5;
+    this.gX=9;
+    this.gY=7;
     this.color=color;
     // this.move=function(){
     //     if(this.positionY-size<=0)
@@ -51,10 +52,7 @@ Ball.prototype.move=function(){
     }
     this.positionX+=this.gX;
     this.positionY-=this.gY;
-    if(this.positionY+this.size>=ctx.canvas.height)
-    {
-        document.querySelector("h1").innerHTML="Przegrales"
-    }
+    
 }
 Ball.prototype.draw=function(){
     ctx.beginPath();
@@ -86,28 +84,29 @@ Net.prototype.draw=function(){
 
 ///VOLLEYBALLER
 const Volleyballer = function(ctx,positionX,color,play){
-    this.height= ctx.canvas.height/4;
-    this.width=ctx.canvas.width/20;
+    this.height= ctx.canvas.height/10;
+    this.width=ctx.canvas.width/10;
     this.positionX=positionX;
     this.positionY= ctx.canvas.height;
     this.color=color;
     this.play=play;
 }
 
-Volleyballer.prototype.move=function(ball){
-   
-        canvas.addEventListener("mousemove",function(e){
-        this.positionX=e.clientX-ctx.canvas.offsetLeft-(this.width/2);
+Volleyballer.prototype.move=function(e){  
         
-        }.bind(this))
-        
-        if((ball.positionX>=this.positionX) && (ball.positionX<=this.positionX+this.width)&&(ball.positionY+ball.size>=this.positionY-this.height))
-        {
-            
-            ball.gX=-ball.gX;
-            ball.gY=-ball.gY;
-        }
+        this.positionX=e-ctx.canvas.offsetLeft-(this.width/2); 
+        console.log(this.width/2);      
     
+}
+
+Volleyballer.prototype.detected=function(ball)
+{
+    if((ball.positionX>=this.positionX) && (ball.positionX<=this.positionX+this.width)&&(ball.positionY+ball.size>=this.positionY-this.height))
+    {
+        
+        ball.gX=-ball.gX;
+        ball.gY=-ball.gY;
+    }
 }
     
 Volleyballer.prototype.draw=function(){
@@ -136,10 +135,10 @@ VolleyballGame.prototype.drawElements=function(){
     this.player.draw();
     this.computer.draw();
 }
-VolleyballGame.prototype.moveElements=function(ball){
-    this.player.move(this.ball);
+VolleyballGame.prototype.moveElements=function(position){
+    this.player.move(position);
     this.ball.move();
-    this.computer.move(this.ball);
+    this.computer.move(position);
     
 }
 
@@ -155,16 +154,23 @@ VolleyballGame.prototype.detectedHits=function(){
         this.computer.positionX=this.net.positionX+this.net.width;
         
     }
-    
 
+    
+    
+    this.computer.detected(this.ball);
+    this.player.detected(this.ball);
    
     
 }
 
 VolleyballGame.prototype.gameOver=function(){
-    if(this.ball.positionY==ctx.canvas.height)
+    if(this.ball.positionY>=ctx.canvas.height)
     {
-        clearInterval(sI);
+        if(this.ball.positionY+this.ball.size>=ctx.canvas.height)
+    {
+        document.querySelector("h1").innerHTML="Przegrales"
+    }
+       clearInterval(sI);
     }
 }
     
@@ -173,17 +179,20 @@ if(canvas && canvas.getContext('2d'))
 {
   const ball1 = new Ball(ctx,10,"blue");  
   const net = new Net(ctx);  
-  const player = new Volleyballer(ctx,100,"red",true);
+  const player = new Volleyballer(ctx,200,"red",true);
   const computer = new Volleyballer(ctx,700,"green",false);
   const game =new VolleyballGame(ctx,ball1,player,computer,net);
+  canvas.addEventListener("mousemove",function(e){
+    position=e.clientX;
+    
+  })
   const startGame = function()
   {
     game.detectedHits();
     ctx.fillStyle="beige";
     ctx.fillRect(0,0,800,350);
-    game.drawElements();
-    
-   game.moveElements(ball1);
+    game.drawElements();    
+   game.moveElements(position);
    game.gameOver();
     
 
