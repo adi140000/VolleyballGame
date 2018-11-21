d6351ea7fb440654a4536f5a2f08e261fa8e22c3
Adrian Zmuda <adi120.a@gmail.com>
2018-11-21 01:17:13 +0100

    Improved hits ball vs net and ball vs Volleyballer , added buttons in index.html

--- game.js
+++ game.js
@@ -1,50 +1,30 @@
+const newGame = document.querySelector("button.newGame");
+const nextRound = document.querySelector("button.nextRound");
+
 const canvas=document.querySelector(".main-canvas");
 const ctx=canvas.getContext('2d');
 
-canvas.width=800;
-canvas.height=350;
+
+
+canvas.width=window.innerWidth/1.5;
+canvas.height=window.innerHeight/1.5;
 ctx.fillStyle="beige";
-ctx.fillRect(0,0,800,350);
+ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
 let sI=null;
 let position=100;
 
-canvas.addEventListener("click", function(e){
-    if(e.clientY-canvas.offsetTop>ctx.canvas.height/3)
-    console.log(e.clientY);
-})
 
 //BALL
 
 const Ball = function(ctx,size,color){
-    this.size=size;
+    this.size=(ctx.canvas.height/1000)*size;
     this.positionX=(ctx.canvas.width/2);
     this.positionY=(ctx.canvas.height/2*.4);
     this.gX=5;
     this.gY=5;
     this.color=color;
     this.canHits=true;
-    // this.move=function(){
-    //     if(this.positionY-size<=0)
-    //     {
-    //         this.gY=-this.gY;
-    //     }
-    //     if(this.positionX-size<=0 || this.positionX+size>=ctx.canvas.width){
-    //         this.gX=-this.gX;
-    //     }
-    //     this.positionX+=this.gX;
-    //     this.positionY-=this.gY;
-    //     if(this.positionY+size>=ctx.canvas.height)
-    //     {
-    //         document.querySelector("h1").innerHTML="Przegrales"
-    //     }
-    // }
-    // this.draw=function(){
-    //     ctx.beginPath();
-    //     ctx.fillStyle=this.color;       
-    //    ctx.arc(this.positionX,this.positionY,size,2*Math.PI,0);
-    //     //ctx.fillRect(this.positionX,this.positionY,this.size,this.size);
-    //     ctx.fill();
-    // }
+   
     
 }
 
@@ -59,9 +39,10 @@ Ball.prototype.move=function(ctx){
     }
     this.positionX+=this.gX;
     this.positionY-=this.gY;
-    if(this.positionY==ctx.canvas.height/2)
+    if(this.positionY<=ctx.canvas.height/2)
     {
         this.canHits=true;
+        
     }
     
 }
@@ -76,20 +57,30 @@ Ball.prototype.draw=function(){
 }
 
 Ball.prototype.detectedNet=function(net){
-    if(this.positionX+this.size>=net.positionX && this.positionX+this.size<=net.positionX+net.width && this.positionY>=net.positionY  )
+    if(this.positionX+this.size>=net.positionX && this.positionX+this.size<=net.positionX+net.width && this.positionY+this.size>=net.positionY && net.hit )
+    {
+        
+        this.gX=-this.gX;
+        net.hit=false;
+
+    }else if(this.positionX-this.size<=net.positionX+net.width && this.positionX-this.size>=net.positionX && this.positionY+this.size>=net.positionY && net.hit)
     {
         this.gX=-this.gX;
+        net.hit=false;
+    }
+    else{
+        net.hit=true;
     }
     
 }
 
 /// NET
 const Net=function(ctx){
-    this.height=(ctx.canvas.height/3*2);;
+    this.height=(ctx.canvas.height/10*5);;
     this.width=6;
     this.positionX=(ctx.canvas.width/2-2);
-    this.positionY=(ctx.canvas.height/3);
-    
+    this.positionY=(ctx.canvas.height/10*5);
+    this.hit=true;
 
 }
  
@@ -103,8 +94,8 @@ Net.prototype.draw=function(){
 
 ///VOLLEYBALLER
 const Volleyballer = function(ctx,positionX,color,play){
-    this.height= ctx.canvas.height/10;
-    this.width=ctx.canvas.width/10;
+    this.height= ctx.canvas.height/16;
+    this.width=ctx.canvas.width/15;
     this.positionX=positionX;
     this.positionY= ctx.canvas.height;
     this.color=color;
@@ -128,7 +119,8 @@ Volleyballer.prototype.detected=function(ball)
         else{
             ball.gX=-ball.gX;
         }       
-        ball.gY=-ball.gY;
+        ball.gY=-ball.gY;        
+        console.log(ball.gY);
         ball.canHits=false;
     }
 }
@@ -179,65 +171,134 @@ VolleyballGame.prototype.detectedHits=function(){
         
     }
 
-    
+    if(this.player.positionX<=0)
+    {
+        this.player.positionX=0;
+    }
+
+    if(this.computer.positionX+this.computer.width>= ctx.canvas.width)
+    {
+        
+        this.computer.positionX=ctx.canvas.width-this.computer.width;
+    }
     
     this.computer.detected(this.ball);
     this.player.detected(this.ball);
     this.ball.detectedNet(this.net);
+
+    if(this.net.positionX<this.ball.positionX+this.ball.size || this.net.positionX+this.net.width>this.ball.positionX-this.ball.size)
+    {
+        this.net.hit=true;        
+    }
    
+    if(this.ball.positionY<=this.ball.size){
+        if(this.ball.gY<10)
+        {
+          this.ball.gY+=0.3;
+          this.ball.gX+=0.3;
+          console.log("przyspieszylem");
+        }
+
+    }
+    
     
 }
 
 VolleyballGame.prototype.gameOver=function(){
-    if(this.ball.positionY>=ctx.canvas.height)
+    if(this.ball.positionY+this.ball.size>=ctx.canvas.height)
     {
        
-    document.querySelector("h1").innerHTML="Przegrales";
-    if(this.ball.positionX<=this.ctx.canvas.width/2)
-    {
-       
-        this.computer.pkt++
-        document.querySelector("div.result-computer").innerHTML=this.computer.pkt;
-    }
-    else
-    {
-        this.player.pkt++
-        document.querySelector("div.result-player").innerHTML=this.player.pkt;
-    }
+    
+        if(this.ball.positionX<=this.ctx.canvas.width/2)
+        {
+        
+            this.computer.pkt++;
+            document.querySelector("div.result-computer").innerHTML=this.computer.pkt;
+        }
+        else
+        {
+            this.player.pkt++;
+            document.querySelector("div.result-player").innerHTML=this.player.pkt;
+        }
 
-    clearInterval(sI);
+        clearInterval(sI);
+        if(this.player.pkt==10)
+        {
+            document.querySelector("h1").innerHTML="Wygral zawodnik nr : 1";
+        }else if(this.computer.pkt==10){
+            document.querySelector("h1").innerHTML="Wygral zawodnik nr : 2";
+        }else{
+            nextRound.style.display="block";
+        }
+        
+        newGame.style.display="block";
+    
     }
 }
-    
 
-if(canvas && canvas.getContext('2d'))
-{
-  const ball1 = new Ball(ctx,14,"yellow");  
-  const net = new Net(ctx);  
-  const player = new Volleyballer(ctx,200,"red",true);
-  const computer = new Volleyballer(ctx,700,"green",false);
-  const game =new VolleyballGame(ctx,ball1,player,computer,net);
-  canvas.addEventListener("mousemove",function(e){
-    position=e.clientX;
-    
-  })
-  const startGame = function()
-  {
-    game.detectedHits();
-    ctx.fillStyle="beige";
-    ctx.fillRect(0,0,800,350);
-    game.drawElements();    
-   game.moveElements(position,ctx);
-   game.gameOver();
-    
 
-  }
-  
-sI=setInterval(startGame,1000/60);
+const ball1 = new Ball(ctx,20,"yellow");  
+const net = new Net(ctx);  
+const player = new Volleyballer(ctx,200,"red",true);
+const computer = new Volleyballer(ctx,700,"green",false);
+const game =new VolleyballGame(ctx,ball1,player,computer,net);
+
+const play = function(){
+    if(canvas && canvas.getContext('2d'))
+      {     
+        ball1.positionX=(ctx.canvas.width/2);
+        ball1.positionY=(ctx.canvas.height/2*.4);   
+        ball1.gX=5;
+        ball1.gY=5;   
+        newGame.style.display="none";
+        nextRound.style.display="none"; 
+     
+      let tmp = Math.round(Math.random());
+      if(tmp)
+      {
+          ball1.gX=-ball1.gX;
+      }
+     
+      canvas.addEventListener("mousemove",function(e){
+        position=e.clientX;
+        
+      })
+      const startGame = function()
+      {
+        game.detectedHits();
+        ctx.fillStyle="beige";
+        ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
+        game.drawElements();    
+       game.moveElements(position,ctx);
+       game.gameOver();      
+
+    
+      }
+      
+    sI=setInterval(startGame,1000/60);   
+    
+    }
+}
+
+
+    nextRound.addEventListener("click",play);
+    newGame.addEventListener("click",play);
+    newGame.addEventListener("click",function(){
+    
+        player.pkt=0;
+        computer.pkt=0
+        
+        document.querySelector("div.result-player").innerHTML=0;
+        document.querySelector("div.result-computer").innerHTML=0;
+        
+    });
+   
+
  
     
 
-}
+
+
 
 
 
